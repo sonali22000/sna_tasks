2e7c32ab26810ae0b350723ef13b06051f7569e3
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-02-01 10:29:20 +0200

    Initial commit

---
+++ contactus.php
@@ -0,0 +1,7 @@
+<?php 
+$title = "Contact Us :HAMK oy";
+include "header.php"?>
+<h2> Contact Us </h2>
+<p> orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker incl
+</p>
+<?php include "footer.php"?>

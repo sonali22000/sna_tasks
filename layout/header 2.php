e918525538306783ad161c77e36cdd4ba1ee6053
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-02-02 13:22:11 +0200

    task 2 submited

---
+++ layout/header 2.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Enroll Project@Team 18</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+ 
+    <style>
+<body>
+    <style>
+        body {
+    background-color:#EF92D5;
+    font-family:'Courier New', Courier, monospace;
+    color:#680a11;
+    font-size: 13.5px;
+    width: 1080px;
+    margin:auto;
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+}
+img{
+    margin-right: 50px;
+    margin-left:5px;
+    margin-bottom: 0px;
+}
+nav li {
+    font-size: 20px;
+    display:inline-block;
+    margin-right:60.5px;
+    margin-left: 10px;
+    margin-bottom:0px;
+}
+nav li:last-child {
+    margin-right: 0px;
+}
+nav ul {
+    padding-left: 0;    
+}
+/*h1{
+    font-size: 20px;
+    text-align: justify;
+}*/
+.header-image {
+    width: 100%;
+    padding-top: 0px;
+    padding-bottom:20px ;
+    padding-left: 0px;
+    padding-right: 0PX;
+}
+h2{
+    background-color:#EF92D5;
+    /*height: 107px;*/
+    padding-top: 0px;
+    padding-bottom:10px ;
+    padding-left: 20px;
+    text-align: center;
+    font-size: 22px;
+
+}
+h3{
+    background-color:#EF92D5;
+    padding-top: 0px;
+    padding-bottom:0px ;
+    padding-left: 20px;
+    text-align: center;
+    font-size: 15px;
+}
+h4{
+    background-color:#EF92D5;
+    padding-top: 0px;
+    padding-bottom:40px ;
+    padding-left: 20px;
+    text-align: center;
+    font-size: 18px;
+
+}
+    
+      
+</style>
+    
+  <div class="container">
+    <header class="site_header">
+        <div class="wrapper"></div>
+        <nav>
+
+            <ul>
+                <li><a href="#"><i><b>Twinkling Stars<br>Kindergarten</a></i></b></li>
+                <img src="https://s3-alpha-sig.figma.com/img/7b4f/4065/9da1c8818617ab8b0b0795c9e548b738?Expires=1675036800&Signature=XBIWY8922dJJdlpFnwhFKn7QSYlmbNEeg~Ii2Hzmw06P5VfdcCQV-Ek27snvBZhECcFnwiaiTggscfn4kznrylCGIyU-dfzCGvxjxlgVbfFTRqaBlJIY0t6KZzb35fS9Gk~ce8jFCLotrTNF9uKzT232HdD91IFvCyZq0WA-teELgOMdoD4qe74Qbgo3hxrPdp~9e-arRx3gySWrG4kQTF0jWgdtM1EEaMjs5IQBDN2iB--vFzz~f1ZN3WFpXrjcOO3Q8ucI4fCuPri4c7fg-9ngXN9zDzivHxwdhIIYM-Lhh0dvg~qyh7Xg0FulyMGB1zEOwNhrtID9t6hwH7TwgA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" class="rounded" alt="Logo" width="100" height="75">
+                <li><a href="indexkushani.html">Home</a></li>
+                <li><a href="activitiesneda.html">Activities</a></li>
+                <li><a href="contactasiri.html">Contact</a></li>
+                <a class="btn btn-danger" href="enrollsonali.html" role="button">Enroll Today</a>
+            </ul>
+                </nav>
+
+</header>
+
+           
+

0b54ab94fb1de13f980a238c7aeb19b4fbd4afdb
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-02-09 22:20:34 +0200

    task submit

--- layout/array.php
+++ layout/array.php
@@ -21,6 +21,8 @@ if (isset($_GET['calculate'])){
             break;
             case "sub":
                 $result = $num1 - $num2;
+
+
                 break;
             case "mul":
                     $result = $num1 * $num2;
@@ -40,8 +42,101 @@ if (isset($result)){
 }
 }
 ?>
+<h4>1.  Write a php script to display courses as list. Use 
+$courses=array("PHP", "HTML", "JavaScript", "CMS", "Project")<li><h4>
+<?php
+$courses1=array("PHP","HTML","JavaScript","CMS","Project");
+unset($courses1["4"]);
+foreach ($courses1 as $value) {
+    echo $value."<br>";
+    var_dump($courses1);}
+    
 
+?>
 
+<h4>2. dump action
+    <?php
+    $courses1=array("PHP","HTML","JavaScript","CMS","Project");
+    unset($courses1["4"]);
+    foreach ($courses1 as $value) {
+        echo $value."<br>";
+        var_dump($courses1);
+        $values=array_values($courses1);
+        foreach ($values as $value) {
+            echo $value . "<br>";}
+        }
+    ?>
+<h4> 3.sorting<h4>
+    <?php
+    $courses3=array("PHP","HTML","JavaScript","CMS","Project");
+    echo "a) ascending order sort by value <br>";
+    asort($courses);
+    foreach ($courses as $x =>$x_value) 
+    {
+        echo $x_value;
+        echo "<br>";
+    }
+    echo "b) ascending order sort by Key>br>";
+    ksort ($courses);
+    foreach($courses as $x=>$x_value)
+    {
+        echo $x_value;
+        echo"<br>";
+    }
+    echo "c) descending order sort by value<br>";
+    arsort($courses);
+    foreach($courses as $x=>$x_value)
+    {
+        echo $x_value;
+        echo "<br>";
+
+    }
+    echo "d) descending order sort by Key<br>";
+    krsort($courses);
+    foreach($courses as $x=>$x_value)
+    {
+        echo $x_value;
+        echo"<br>";
+    }
+    ?>
+<h4>4. all values to upper case<h4>
+    <?php
+    $courses=array("PHP","HTML","JavaScript","CMS","Project");
+    print_r(array_change_key_case($course,CASE_UPPER));
+    ?>
+<h4>5.List all your favorite colors and their hexadecimal equivalents<h4>
+
+<?php
+$color['#ff0000'] = "Red";
+$color['#ffc0cb'] = "Pink";
+$color['#0000ff'] = "Blue";
+$color['#800080'] = "Purple";
+$color['#008000'] = "Green";
+
+foreach($color as $x=>$x_values) {
+    echo $x. "=>".$x_values."<br>";}
+    ?>
+<h4>6.PHP script to calculate and display average temperature<h4>
+    <?php
+    $a=array (78, 60, 62, 68, 71, 68, 73,
+    85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 
+    74, 62, 62, 65, 64, 68, 73, 75, 79, 73
+);
+$sum=array_sum($a);
+$count=count($a);
+echo"Avarage temperature : ".$sum/$count;
+
+sort($a);
+
+for($i=0; $i <5; $i++){
+    echo $a[$i]."<br>";}
+    rsort($a);
+    for ($i=0; $i < 5; $i++){
+        echo $a[$i]."<br>";}
+?>
+}
+
+}
 
 
 

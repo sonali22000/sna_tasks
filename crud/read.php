150a65319460fc3f4cbdfcf96fec932714e26f33
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-02-15 22:46:09 +0200

    somthing

---
+++ crud/read.php
@@ -0,0 +1,35 @@
+<?php
+$title = "Reading Data from the database";
+include '../layout/header.php';
+include 'db.php';
+$sql = "select*from studentinfo";
+$result = $conn->query($sql);
+if($result->num_rows > 0){
+    echo "<table class='table'>
+<tr><th>ID</th><th>First Name </th><th>Last Name</th>
+<th>City</th><th>Group ID</th></tr>";
+    while ($row = $result ->fetch_assoc()){
+echo"
+
+<tr>
+<td>$row[id]</td>
+<td>$row[fname]</td>
+<td>$row[lname]</td>
+<td>$row[city]</td>
+<td>$row[groupid]</td>
+</tr>
+";
+    }
+echo "</table>";
+}
+
+else
+{
+    echo "NO Results";
+}
+$conn->close();
+include '../layout/footer.php';
+
+
+
+?>

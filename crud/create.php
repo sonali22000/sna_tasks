740e654e8291dc8182fe58ffe0abb678e114d088
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-02-22 14:44:04 +0200

    inclass

--- crud/create.php
+++ crud/create.php
@@ -2,8 +2,8 @@
 $title = "A simple CRUD app";
 include '../layout/header.php';
 ?>
-<form method="post" action="">
-    <input type ="text"name="fname" placeholder="First Name" required ><br><br>
+<form method="post" action="" onsubmit="return crud()" name="form1">
+    <input type ="text"name="fname" placeholder="First Name" required onblur="fname()"> <br><br>
     <input type ="text"name="lname" placeholder="Last Name" required ><br><br>
     <input type ="text"name="city" placeholder="City" required ><br><br>
     <select name="groupid">
